2f59892e0bc82a8d0300a960ca1a0d2e1a269492
ZHUOMA <markus.zhuo@oocl.com>
2019-07-18 12:30:53 +0800

    类成员名字修改

---
+++ src/test/java/com/oocl/grow/integration/GoalsApiTest.java
@@ -0,0 +1,49 @@
+package com.oocl.grow.integration;
+
+import com.oocl.grow.GrowApplication;
+import com.oocl.grow.model.Goal;
+import com.oocl.grow.repository.GoalRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = GrowApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class GoalsApiTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private GoalRepository repository;
+
+    @Test
+    public void givenGoals_whenGetGoals_thenStatus200()
+            throws Exception {
+
+        createTestGoal("100001");
+
+        mvc.perform(get("/goal/get_all_goals")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].goal_id", is("100001")));
+    }
+
+    private void createTestGoal(String goal_id) {
+        Goal goal = Goal.builder().goalId(goal_id).build();
+        repository.saveAndFlush(goal);
+    }
+}
